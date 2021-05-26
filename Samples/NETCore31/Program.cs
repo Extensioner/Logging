7f7942a3c963cd235819bda1cfdc9b2c09348c4e
Dmr <davidyamer@outlook.com>
2021-05-26 15:42:15 +0800

    添加测试代码测试.net standard类库的兼容性

---
+++ Samples/NETCore31/Program.cs
@@ -0,0 +1,14 @@
+﻿using Extensioner.Logging;
+using System;
+
+namespace NETCore31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Debug("Hello NETCore3.1");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
